463417568cf6c90bdf9c79ea2b86dd47a76ea0b4
tolunayozturk <ozturktolunay@hotmail.com>
2018-01-18 19:36:16 +0300

    Initial Commit

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShooting : NetworkBehaviour 
+{
+	[SerializeField] float shotCooldown = .3f;
+	[SerializeField] Transform firePosition;
+	[SerializeField] ShotEffectsManager shotEffects;
+
+	float ellapsedTime;
+	bool canShoot;
+
+	void Start()
+	{
+		if (isLocalPlayer)
+			canShoot = true;
+	}
+
+	void Update()
+	{
+		if (!canShoot)
+			return;
+
+		ellapsedTime += Time.deltaTime;
+
+		if (Input.GetButtonDown ("Fire1") && ellapsedTime > shotCooldown) 
+		{
+			ellapsedTime = 0f;
+			CmdFireShot (firePosition.position, firePosition.forward);
+		}
+	}
+
+	[Command]
+	void CmdFireShot(Vector3 origin, Vector3 direction)
+	{
+		RaycastHit hit;
+
+		Ray ray = new Ray (origin, direction);
+		Debug.DrawRay (ray.origin, ray.direction * 3f, Color.red, 1f);
+
+		bool result = Physics.Raycast (ray, out hit, 50f);
+
+		if (result) 
+		{
+			PlayerHealth enemy = hit.transform.GetComponent<PlayerHealth> ();
+
+			if (enemy != null)
+				enemy.TakeDamage ();
+		}
+
+		RpcProcessShotEffects (result, hit.point);
+	}
+
+	[ClientRpc]
+	void RpcProcessShotEffects(bool playImpact, Vector3 point)
+	{
+		shotEffects.Initialize ();
+		shotEffects.PlayShotEffects ();
+
+		if (playImpact)
+			shotEffects.PlayImpactEffect (point);
+	}
+}
